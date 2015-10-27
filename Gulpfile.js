859642f33cfefcd5c3f34ee92067784fa16320a8
CharlesAnthonyBrowne <charles@charlesbrowne.io>
2015-10-27 10:04:40 +0000

    gulpfile and dependencies

---
+++ Gulpfile.js
@@ -0,0 +1,45 @@
+var
+		gulp           = require('gulp');
+		babel          = require("gulp-babel");
+    uglify         = require('gulp-uglify');
+		postcss        = require('gulp-postcss');
+	  vars           = require('postcss-simple-vars');
+		postcss        = require('gulp-postcss');
+	  concat         = require('gulp-concat');
+		url            = require("postcss-url");
+		sourcemaps     = require('gulp-sourcemaps');
+		autoprefixer   = require('autoprefixer');
+		atImport       = require("postcss-import");
+		processors     = [
+										 require('postcss-mixins'),
+										 require('postcss-simple-vars'),
+										 require('postcss-nested'),
+										 require('autoprefixer')({ browsers: ['last 2 versions', '> 2%'] })
+		];
+
+gulp.task("babel", function () {
+  return gulp.src("./javascripts/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("main.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./bundle/"));
+});
+
+// Watch task
+gulp.task('watch', function(){
+  gulp.watch('./stylesheets/*.css', ['css']);
+	gulp.watch('./javascripts/*.js', ['babel']);
+});
+
+// Compile task
+gulp.task('css', function () {
+    return gulp.src('./stylesheets/*.css')
+        .pipe(sourcemaps.init())
+	      .pipe(postcss(processors))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./bundle/'));
+});
+
+
+gulp.task('default', ["css", "watch", "babel"]);
